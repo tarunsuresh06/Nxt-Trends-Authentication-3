7fa420b36b31ce9bf21c45a827ede1a2885290e9
tommy <tarunsuresh.v@gmail.com>
2023-05-19 10:31:10 +0000

    Nxt Trends Authorization 3

---
+++ src/components/AllProductsSection/index.js
@@ -0,0 +1,70 @@
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+import Cookies from 'js-cookie'
+
+import ProductCard from '../ProductCard'
+import './index.css'
+
+class AllProductsSection extends Component {
+  state = {
+    productsList: [],
+    isLoading: true,
+  }
+
+  componentDidMount() {
+    this.getProducts()
+  }
+
+  getProducts = async () => {
+    const apiUrl = 'https://apis.ccbp.in/products'
+    const jwtToken = Cookies.get('jwt_token')
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+    }
+
+    const response = await fetch(apiUrl, options)
+    if (response.ok === true) {
+      const data = await response.json()
+      const formattedData = data.products.map(product => ({
+        id: product.id,
+        brand: product.brand,
+        price: product.price,
+        rating: product.rating,
+        imageUrl: product.image_url,
+        title: product.title,
+      }))
+
+      this.setState({productsList: formattedData, isLoading: false})
+    }
+  }
+
+  renderProductsList = () => {
+    const {productsList} = this.state
+    return (
+      <div>
+        <h1 className="products-list-heading">All Products</h1>
+        <ul className="products-list">
+          {productsList.map(product => (
+            <ProductCard productData={product} key={product.id} />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+
+  renderLoader = () => (
+    <div className="loader-container">
+      <Loader type="TailSpin" width={80} height={80} color="#475569" />
+    </div>
+  )
+
+  render() {
+    const {isLoading} = this.state
+    return <>{isLoading ? this.renderLoader() : this.renderProductsList()}</>
+  }
+}
+
+export default AllProductsSection
